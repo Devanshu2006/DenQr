fc8c64e0a793de4e1b8ec2cc149138c9aa388a5d
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-05 17:07:58 +0530

    po

--- app.py
+++ app.py
@@ -1102,5 +1102,5 @@ def add_header(response):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port)
