4db9c4a1f7b4450b894bbf1b69e7b34972377854
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-07 02:01:42 +0530

    as

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ with app.app_context():
 
 # SENDGRID_API_KEY = "SG.re_FJhM8V47_9q9ku17hZQZpS7vPuWLYpgNLre_FR3DocJZ_82TQwWAoENyfmZWrHeacbqGP"
 # SENDER_EMAIL = "devanshupawar2006@gmail.com"
-resend.api_key = "re_FJhM8V47_9q9ku17hZQZpS7vPuWLYpgNLre_FR3DocJZ_82TQwWAoENyfmZWrHeacbqGP"
+resend.api_key = "re_bJ379Dea_JsDw4XVHsYXEtUzRnug7w6pi"
 s= URLSafeTimedSerializer(app.secret_key)
 
 client = razorpay.Client(auth=(os.getenv("RAZORPAY_KEY_ID"), os.getenv("RAZORPAY_KEY_SECRET")))
