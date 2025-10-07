fc236c9eda5feec28aa31240d8e6315e9c5a15bc
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-07 22:50:57 +0530

    push

--- app.py
+++ app.py
@@ -661,7 +661,12 @@ def generate_qrs_json():
             image_stream = io.BytesIO(image_bytes)
 
             p.drawImage(
-                image_stream, x_pos, y_pos - qr_size, width=qr_size, height=qr_size
+                image=image_stream,
+                x=x_pos,
+                y=y_pos - qr_size,
+                width=qr_size,
+                height=qr_size,
+                mask='auto'
             )
 
             p.setFont("Helvetica", 10)
