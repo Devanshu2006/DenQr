1337afed33fbf9340c4ca6143e855e4645cf503e
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-30 22:25:57 +0530

    did it

--- app.py
+++ app.py
@@ -26,19 +26,19 @@ from PIL import Image
 
 app = Flask(__name__, template_folder="templates")
 print("Templates folder absolute path:", os.path.abspath(os.path.join(os.getcwd(), "templates")))
-# app.secret_key = os.environ.get('APP_SECRET_KEY')
-app.secret_key = "my_dream_project_of_2006"
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+# app.secret_key = "my_dream_project_of_2006"
 socketio = SocketIO(app)
 
-# DATABASE_URL = "database-1.cbg0qcaqy83i.ap-south-1.rds.amazonaws.com"
-# conn = psycopg2.connect(DATABASE_URL)
-conn = psycopg2.connect(
-    host="oddz.cbg0qcaqy83i.ap-south-1.rds.amazonaws.com",
-    database="oddz",
-    user="postgres",
-    password="my_dream_project_of_2006",
-    port="5432"
-)
+DATABASE_URL = "postgresql://oddz_7d2m_user:XchteBlGGUaBLNnTqBUM55Hw1ap0LRNw@dpg-d3f1mo15pdvs73ccof50-a/oddz_7d2m"
+conn = psycopg2.connect(DATABASE_URL)
+# conn = psycopg2.connect(
+#     host="oddz.cbg0qcaqy83i.ap-south-1.rds.amazonaws.com",
+#     database="oddz",
+#     user="postgres",
+#     password="my_dream_project_of_2006",
+#     port="5432"
+# )
 cur = conn.cursor()
 # cur = conn.cursor()
 
