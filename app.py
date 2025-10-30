8cfde50fb4798c41838b70743a3209ee761ac7c7
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-30 22:58:34 +0530

    done dona done

--- app.py
+++ app.py
@@ -797,7 +797,7 @@ def generate_pdf(qr_data, restaurants_id, restaurant_name):
         c.rect(x - 5, y - qr_size - 5, qr_size + 10, qr_size + 30)
 
         c.setFont("Helvetica-Bold", 10)
-        c.drawCentredString(x+qr_size/2, y-10, f"DenQr-{restaurant_name}")
+        c.drawCentredString(x+qr_size/2, y-2, f"Scan & Order")
 
         c.setFont("Helvetica", 12)
         c.drawCentredString(x + qr_size / 2, y - qr_size - 20, f"Table: {index + 1}")
