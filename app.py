0129180b605ae659969302eb2eca43456bf7409a
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-31 13:02:28 +0530

    bvn

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ conn = psycopg2.connect(
     host="oddz.cbg0qcaqy83i.ap-south-1.rds.amazonaws.com",
     dbname="postgres",
     user="oddz",
-    password="my_dream_project_of_2006",
+    password="7999178184",
     port="5432"
 )
 cur = conn.cursor()
