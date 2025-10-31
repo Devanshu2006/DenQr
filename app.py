d6275301c18a1f0d4f0ba5d98713104282c4b181
Devanshu Girare <devanshupawar20@gmail.com>
2025-10-31 13:15:18 +0530

    do did

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ cur = conn.cursor()
 
 def init_db():
     cur = conn.cursor()
-    cur.execute("select * from subscriptions")
-    row = cur.fetchall()
-    print(row)
+    # cur.execute("select * from subscriptions")
+    # row = cur.fetchall()
+    # print(row)
     cur.execute("""
 
         CREATE TABLE IF NOT EXISTS admins(
