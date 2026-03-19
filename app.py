b9dfe86502554b6df8666b9b7b7e3777ecda68bf
Devanshu Girare <devanshupawar20@gmail.com>
2026-03-19 22:32:14 +0530

    new database

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.secret_key = os.environ.get('APP_SECRET_KEY')
 # app.secret_key = "my_dream_project_of_2006"
 socketio = SocketIO(app)
 
-DATABASE_URL = "postgresql://denqr:ivei4SQk3M0eYR8jZUr1elUIGoIf9CtP@dpg-d6a81ph4tr6s73d1d0b0-a/oddz_fxde"
+DATABASE_URL = "postgresql://denqr:zpFvQCcVPz4ruOePUQuppZiVAk2Xh11Z@dpg-d6u2mgffte5s73856tjg-a/oddz_bajb"
 conn = psycopg2.connect(DATABASE_URL)
 # conn = psycopg2.connect(
 #     host="oddz.cbg0qcaqy83i.ap-south-1.rds.amazonaws.com",
